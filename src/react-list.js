2b79a7d31c61f0da5db2b5c1dd2ea5e180533b61
jefferchang <changyibiao126@163.com>
2015-11-28 15:16:55 +0800

    添加用户查询列表

--- src/react-list.js
+++ src/react-list.js
@@ -6,8 +6,32 @@ require.config({
         "jquery": "./../bower_components/jquery/dist/jquery"
     }
 });
-define('listModule', ['react-bootstrap', 'react', 'react-dom','jquery'], function (ReactBootstrap, React, ReactDom,$) {
+define('listModule', ['react-bootstrap', 'react', 'react-dom', 'jquery'], function (ReactBootstrap, React, ReactDom, $) {
         var Table = ReactBootstrap.Table;
+
+        const EditUser =React.createClass({
+            editUser:function(e){
+                alert("修改");
+             },
+            render:function(){
+                return (
+                    <a onClick={this.editUser} className="ButtonCursor">修改</a>
+                )
+            }
+        });
+
+    const DeleteUser = React.createClass({
+        deleteUser:function(e){
+            alert("删除信息");
+        },
+        render:function(){
+            return (
+                  <a  onClick={this.deleteUser}  className="ButtonCursor" >删除</a>
+            )
+        }
+    });
+
+
         const TableInstance = React.createClass({
                 render: function () {
                     var listUsers = this.props.data.map(function (user) {
@@ -18,6 +42,7 @@ define('listModule', ['react-bootstrap', 'react', 'react-dom','jquery'], functio
                                 <td>{user.name}</td>
                                 <td>{user.comment}</td>
                                 <td>{user.age}</td>
+                                <td><EditUser/>,<DeleteUser/></td>
                             </tr>
                         );
                     });
@@ -30,6 +55,7 @@ define('listModule', ['react-bootstrap', 'react', 'react-dom','jquery'], functio
                                 <th>姓名</th>
                                 <th>备注</th>
                                 <th>年龄</th>
+                                <th>操作</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -41,33 +67,45 @@ define('listModule', ['react-bootstrap', 'react', 'react-dom','jquery'], functio
             }
         );
         const UserList = React.createClass({
-            getInitialState: function() {
+            getInitialState : function () {
                 return {data: []};
             },
-            loadUsersFromServer:function(){
+            loadUsersFromServer: function () {
                 $.ajax({
                     url: this.props.url,
                     dataType: 'json',
                     cache: false,
-                    success: function(data) {
+                    success: function (data) {
                         this.setState({data: data});
                     }.bind(this),
-                    error: function(xhr, status, err) {
+                    error: function (xhr, status, err) {
                         console.error(this.props.url, status, err.toString());
                     }.bind(this)
                 });
             },
-            componentDidMount: function() {
+            componentDidMount: function () {
                 this.loadUsersFromServer();
             },
-            render :function(){
-                return(
-                    <TableInstance data={this.state.data} />
-                    );
+            render: function () {
+                return (
+                    <TableInstance data={this.state.data}/>
+                );
             }
         });
 
-        ReactDom.render(<UserList url="/api/users.json"  />, document.getElementById("app"));
+
+        var Panel = ReactBootstrap.Panel;
+        const PanelInstance = React.createClass({
+                render: function () {
+                    return (
+                        <Panel  header="用户列表" bsStyle="primary">
+                            <UserList url="/api/users.json"/>
+                        </Panel>
+                    )
+                }
+            }
+        );
+        ReactDom.render(<PanelInstance />, document.getElementById("app"));
     }
 );
 require(["listModule"]);
